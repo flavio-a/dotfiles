cabf49e1663aaf81286fa20b50adcb7b193a7a07
Flavio <flavio.a.618@gmail.com>
2020-05-14 20:45:10 +0200

    Added things for byobu and uxterm

--- dotfiles/emacs.d/init.el
+++ dotfiles/emacs.d/init.el
@@ -1,8 +1,14 @@
 ;; backup in one place. flat, no tree structure
-(setq backup-directory-alist '(("" . "~/.emacs.d/backup")))
+(setq backup-directory-alist '(("" . "/dev/shm/emacs-backup")))
 
 ;; lua-mode
 (add-to-list 'load-path "/home/flavio/.emacs.d/elpa")
 (autoload 'lua-mode "lua-mode" "Lua editing mode." t)
 (add-to-list 'auto-mode-alist '("\\.lua$" . lua-mode))
 (add-to-list 'interpreter-mode-alist '("lua" . lua-mode))
+(setq lua-indent-level 4)
+
+;; tab
+(setq-default indent-tabs-mode nil)
+(setq-default tab-width 4)
+(defvaralias 'c-basic-offset 'tab-width)
