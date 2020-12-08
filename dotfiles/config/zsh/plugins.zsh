8299f226234daa9fed638d3c44964365285628be
Flavio <flavio.a.618@gmail.com>
2020-12-08 17:11:53 +0100

    Added wd

--- dotfiles/config/zsh/plugins.zsh
+++ dotfiles/config/zsh/plugins.zsh
@@ -9,6 +9,10 @@ ZSH_AUTOSUGGEST_STRATEGY='match_prev_cmd'
 ZSH_AUTOSUGGEST_BUFFER_MAX_SIZE=20
 ZSH_AUTOSUGGEST_USE_ASYNC=true
 
+# wd
+
+WD_CONFIG="$HOME/.cache/warprc"
+
 # Load
 
 source "$ZDOTDIR/zsh_plugins_pre.sh"
