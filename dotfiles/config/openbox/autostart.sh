378d577ec45f135dbc990d7e4d96a48915e8ed9a
Flavio <flavio.a.618@gmail.com>
2021-03-02 18:08:10 +0100

    Adding caffeine

--- dotfiles/config/openbox/autostart.sh
+++ dotfiles/config/openbox/autostart.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
 sh ${HOME}/.config/openbox/mylayout.sh
+tint2 &
 numlockx &
 sh ${HOME}/.config/Xenv/xinitrc
 uxterm -e "byobu; $SHELL" &
-xscreensaver &
 sh ~/.config/openbox/carica_sfondo.sh
-tint2 &
 sh ~/.config/openbox/start-tg.sh &
+xscreensaver &
