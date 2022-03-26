ee6ad5893e532f095f223ad2bd7c00c9f44b38da
Flavio <flavio.a.618@gmail.com>
2022-03-26 16:31:56 +0100

    Generic config updates

--- dotfiles/config/openbox/autostart.sh
+++ dotfiles/config/openbox/autostart.sh
@@ -3,9 +3,8 @@
 obconfdir="${HOME}/.config/openbox"
 
 autorandr --change --force
-tint2 &
 numlockx &
-xrdb ~/.config/Xenv/Xresources
+# xrdb ~/.config/Xenv/Xresources
 uxterm -e "byobu" &
 sh ${obconfdir}/start-tg.sh &
 xscreensaver &
