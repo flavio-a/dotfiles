a8b12f1d05819d8b7ad2314dfbfc1bac25cd93b9
Flavio <flavio.a.618@gmail.com>
2023-09-02 14:31:12 +0200

    Moving wallpapers images and scripts to a separate dir

---
+++ dotfiles/config/wallpapers/load_wallpaper.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+BG_DIR="$HOME/.config/wallpapers/images/"
+HEAD1="default"
+
+if [ -f "$BG_DIR/$HOSTNAME" ]; then
+    HEAD1="$HOSTNAME"
+fi
+
+# Pick a wallpaper for desktop 0!
+HEAD0="$HEAD1"
+
+nitrogen --head=0 --set-zoom "$BG_DIR/$HEAD0"
+nitrogen --head=1 --set-zoom "$BG_DIR/$HEAD1"
